eda526f134d14bfab6984daefa914b56cf7c821a
Pallavi Singh3 <PS00719935@techmahindra.com>
2020-12-21 01:00:23 +0530

    contact Us form validation done

--- src/app/layout/contact-us/contact-us.component.ts
+++ src/app/layout/contact-us/contact-us.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent{
+  
+  @ViewChild('contactForm') formDetails : NgForm; 
 
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  submitContactForm = function(){
+    console.log('contactForm called', this.formDetails);
   }
 
 }
